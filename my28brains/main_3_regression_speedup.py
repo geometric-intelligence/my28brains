f587081952352b1d06ba3b398909893e18b80945
Adele Myers <adele@frank.ece.ucsb.edu>
2023-06-12 16:11:43 -0700

    trying to export synthetic data to data folder so that we do not have to create each time. still debugging

--- my28brains/main_3_regression_speedup.py
+++ my28brains/main_3_regression_speedup.py
@@ -69,23 +69,117 @@ from my28brains.discrete_surfaces import DiscreteSurfaces, ElasticMetric
 data_type = default_config.data_type
 sped_up = default_config.sped_up
 
+##################### Load Data (or create synthetic data) #####################
+
 if data_type == "synthetic":
     print("Using synthetic data")
-    mesh_dir = synthetic_data_dir
-    sphere_mesh = generate_syntetic_geodesics.generate_sphere_mesh()
-    ellipsoid_mesh = generate_syntetic_geodesics.generate_ellipsoid_mesh()
-    (
-        mesh_sequence_vertices,
-        mesh_faces,
-        times,
-        true_intercept,
-        true_slope,
-    ) = generate_syntetic_geodesics.generate_synthetic_parameterized_geodesic(
-        sphere_mesh, ellipsoid_mesh
-    )
-    print("Original mesh_sequence vertices: ", mesh_sequence_vertices.shape)
-    print("Original mesh faces: ", mesh_faces.shape)
-    print("Times: ", times.shape)
+    synthetic_mesh_sequence_dir = default_config.synthetic_mesh_sequence_dir
+    start_shape_dir = default_config.start_shape_dir
+    end_shape_dir = default_config.end_shape_dir
+    start_shape = default_config.start_shape
+    end_shape = default_config.end_shape
+    n_times = default_config.n_times
+
+    if data_type == "synthetic":
+
+        if not os.path.exists(synthetic_mesh_sequence_dir):
+            print(
+                f"Creating synthetic geodesic with start_mesh: {start_shape}, end_mesh: {end_shape}, and n_times: {n_times}"
+            )
+
+            if not os.path.exists(start_shape_dir):
+                print(f"Creating {start_shape} mesh in {start_shape_dir}")
+                start_mesh = generate_syntetic_geodesics.generate_synthetic_mesh(
+                    start_shape
+                )
+                start_mesh_vertices = start_mesh.vertices
+                start_mesh_faces = start_mesh.faces
+
+                os.makedirs(start_shape_dir)
+                np.save(
+                    os.path.join(start_shape_dir, "vertices.npy"), start_mesh_vertices
+                )
+                np.save(os.path.join(start_shape_dir, "faces.npy"), start_mesh_faces)
+            else:
+                print(
+                    f"{start_shape} mesh already exists in {start_shape_dir}. Loading now."
+                )
+                start_mesh_vertices = np.load(
+                    os.path.join(start_shape_dir, "vertices.npy")
+                )
+                start_mesh_faces = np.load(os.path.join(start_shape_dir, "faces.npy"))
+                start_mesh = trimesh.Trimesh(
+                    vertices=start_mesh_vertices, faces=start_mesh_faces
+                )
+
+            if not os.path.exists(end_shape_dir):
+                print(f"Creating {end_shape} mesh in {end_shape_dir}")
+                end_mesh = generate_syntetic_geodesics.generate_synthetic_mesh(
+                    end_shape
+                )
+                end_mesh_vertices = end_mesh.vertices
+                end_mesh_faces = end_mesh.faces
+
+                os.makedirs(end_shape_dir)
+                np.save(os.path.join(end_shape_dir, "vertices.npy"), end_mesh_vertices)
+                np.save(os.path.join(end_shape_dir, "faces.npy"), end_mesh_faces)
+            else:
+                print(
+                    f"{end_shape} mesh already exists in {end_shape_dir}. Loading now."
+                )
+                end_mesh_vertices = np.load(os.path.join(end_shape_dir, "vertices.npy"))
+                end_mesh_faces = np.load(os.path.join(end_shape_dir, "faces.npy"))
+                end_mesh = trimesh.Trimesh(
+                    vertices=end_mesh_vertices, faces=end_mesh_faces
+                )
+
+            (
+                mesh_sequence_vertices,
+                mesh_faces,
+                times,
+                true_intercept,
+                true_slope,
+            ) = generate_syntetic_geodesics.generate_synthetic_parameterized_geodesic(
+                start_mesh, end_mesh, n_times
+            )
+            print("Original mesh_sequence vertices: ", mesh_sequence_vertices.shape)
+            print("Original mesh faces: ", mesh_faces.shape)
+            print("Times: ", times.shape)
+
+            os.makedirs(synthetic_mesh_sequence_dir)
+            np.save(
+                os.path.join(synthetic_mesh_sequence_dir, "mesh_sequence_vertices.npy"),
+                mesh_sequence_vertices,
+            )
+            np.save(
+                os.path.join(synthetic_mesh_sequence_dir, "mesh_faces.npy"), mesh_faces
+            )
+            np.save(os.path.join(synthetic_mesh_sequence_dir, "times.npy"), times)
+            np.save(
+                os.path.join(synthetic_mesh_sequence_dir, "true_intercept.npy"),
+                true_intercept,
+            )
+            np.save(
+                os.path.join(synthetic_mesh_sequence_dir, "true_slope.npy"), true_slope
+            )
+
+        else:
+            print(
+                f"Synthetic geodesic ALREADY EXISTS with start mesh {start_shape}, end mesh {end_shape}, and n_times {n_times}. Loading now."
+            )
+            mesh_sequence_vertices = np.load(
+                os.path.join(synthetic_mesh_sequence_dir, "mesh_sequence_vertices.npy")
+            )
+            mesh_faces = np.load(
+                os.path.join(synthetic_mesh_sequence_dir, "mesh_faces.npy")
+            )
+            times = np.load(os.path.join(synthetic_mesh_sequence_dir, "times.npy"))
+            true_intercept = np.load(
+                os.path.join(synthetic_mesh_sequence_dir, "true_intercept.npy")
+            )
+            true_slope = np.load(
+                os.path.join(synthetic_mesh_sequence_dir, "true_slope.npy")
+            )
 
 elif data_type == "real":
     print("Using real data")
