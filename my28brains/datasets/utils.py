c2266f5e5783b6d29003799de952c3f956f0a688
Nina Miolane <nmiolane@gmail.com>
2023-06-13 15:39:56 -0700

    Format cleaning in utils.py

--- my28brains/datasets/utils.py
+++ my28brains/datasets/utils.py
@@ -13,14 +13,19 @@ def load(config):
     """Load data according to values in config file."""
     if config.data_type == "synthetic":
         print("Using synthetic data")
-        synthetic_mesh_sequence_dir = config.synthetic_mesh_sequence_dir
+        mesh_dir = config.synthetic_mesh_sequence_dir
         start_shape_dir = config.start_shape_dir
         end_shape_dir = config.end_shape_dir
         start_shape = config.start_shape
         end_shape = config.end_shape
         n_times = config.n_times
 
-        if not os.path.exists(synthetic_mesh_sequence_dir):
+        start_vertices_path = os.path.join(start_shape_dir, "vertices.npy")
+        start_faces_path = os.path.join(start_shape_dir, "faces.npy")
+        end_vertices_path = os.path.join(end_shape_dir, "vertices.npy")
+        end_faces_path = os.path.join(end_shape_dir, "faces.npy")
+
+        if not os.path.exists(mesh_dir):
             print(
                 "Creating synthetic geodesic with"
                 f" start_mesh: {start_shape}, end_mesh: {end_shape},"
@@ -34,16 +39,12 @@ def load(config):
                 start_mesh_faces = start_mesh.faces
 
                 os.makedirs(start_shape_dir)
-                np.save(
-                    os.path.join(start_shape_dir, "vertices.npy"), start_mesh_vertices
-                )
-                np.save(os.path.join(start_shape_dir, "faces.npy"), start_mesh_faces)
+                np.save(start_vertices_path, start_mesh_vertices)
+                np.save(start_faces_path, start_mesh_faces)
             else:
                 print(f"{start_shape} mesh exists in {start_shape_dir}. Loading now.")
-                start_mesh_vertices = np.load(
-                    os.path.join(start_shape_dir, "vertices.npy")
-                )
-                start_mesh_faces = np.load(os.path.join(start_shape_dir, "faces.npy"))
+                start_mesh_vertices = np.load(start_vertices_path)
+                start_mesh_faces = np.load(start_faces_path)
                 start_mesh = trimesh.Trimesh(
                     vertices=start_mesh_vertices, faces=start_mesh_faces
                 )
@@ -55,12 +56,12 @@ def load(config):
                 end_mesh_faces = end_mesh.faces
 
                 os.makedirs(end_shape_dir)
-                np.save(os.path.join(end_shape_dir, "vertices.npy"), end_mesh_vertices)
-                np.save(os.path.join(end_shape_dir, "faces.npy"), end_mesh_faces)
+                np.save(end_vertices_path, end_mesh_vertices)
+                np.save(end_faces_path, end_mesh_faces)
             else:
                 print(f"{end_shape} mesh exists in {end_shape_dir}. Loading now.")
-                end_mesh_vertices = np.load(os.path.join(end_shape_dir, "vertices.npy"))
-                end_mesh_faces = np.load(os.path.join(end_shape_dir, "faces.npy"))
+                end_mesh_vertices = np.load(end_vertices_path)
+                end_mesh_faces = np.load(end_faces_path)
                 end_mesh = trimesh.Trimesh(
                     vertices=end_mesh_vertices, faces=end_mesh_faces
                 )
@@ -78,22 +79,18 @@ def load(config):
             print("Original mesh faces: ", mesh_faces.shape)
             print("Times: ", times.shape)
 
-            os.makedirs(synthetic_mesh_sequence_dir)
+            os.makedirs(mesh_dir)
             np.save(
-                os.path.join(synthetic_mesh_sequence_dir, "mesh_sequence_vertices.npy"),
+                os.path.join(mesh_dir, "mesh_sequence_vertices.npy"),
                 mesh_sequence_vertices,
             )
+            np.save(os.path.join(mesh_dir, "mesh_faces.npy"), mesh_faces)
+            np.save(os.path.join(mesh_dir, "times.npy"), times)
             np.save(
-                os.path.join(synthetic_mesh_sequence_dir, "mesh_faces.npy"), mesh_faces
-            )
-            np.save(os.path.join(synthetic_mesh_sequence_dir, "times.npy"), times)
-            np.save(
-                os.path.join(synthetic_mesh_sequence_dir, "true_intercept.npy"),
+                os.path.join(mesh_dir, "true_intercept.npy"),
                 true_intercept,
             )
-            np.save(
-                os.path.join(synthetic_mesh_sequence_dir, "true_coef.npy"), true_coef
-            )
+            np.save(os.path.join(mesh_dir, "true_coef.npy"), true_coef)
             return mesh_sequence_vertices, mesh_faces, times, true_intercept, true_coef
 
         else:
@@ -103,24 +100,14 @@ def load(config):
                 f" and n_times {n_times}. Loading now."
             )
             mesh_sequence_vertices = gs.array(
-                np.load(
-                    os.path.join(
-                        synthetic_mesh_sequence_dir, "mesh_sequence_vertices.npy"
-                    )
-                )
+                np.load(os.path.join(mesh_dir, "mesh_sequence_vertices.npy"))
             )
-            mesh_faces = gs.array(
-                np.load(os.path.join(synthetic_mesh_sequence_dir, "mesh_faces.npy"))
-            )
-            times = gs.array(
-                np.load(os.path.join(synthetic_mesh_sequence_dir, "times.npy"))
-            )
+            mesh_faces = gs.array(np.load(os.path.join(mesh_dir, "mesh_faces.npy")))
+            times = gs.array(np.load(os.path.join(mesh_dir, "times.npy")))
             true_intercept = gs.array(
-                np.load(os.path.join(synthetic_mesh_sequence_dir, "true_intercept.npy"))
+                np.load(os.path.join(mesh_dir, "true_intercept.npy"))
             )
-            true_coef = gs.array(
-                np.load(os.path.join(synthetic_mesh_sequence_dir, "true_coef.npy"))
-            )
+            true_coef = gs.array(np.load(os.path.join(mesh_dir, "true_coef.npy")))
             return mesh_sequence_vertices, mesh_faces, times, true_intercept, true_coef
 
     elif config.data_type == "real":
