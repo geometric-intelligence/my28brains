a1211a4edd4f6588a6f7a0cf42e99e7de7b3b7ce
Nina Miolane <nmiolane@gmail.com>
2023-06-29 16:51:57 +0200

    Add wandb run for comparison linear vs geodesic square distances

--- my28brains/main_5_linear_vs_geodesic_distances.py
+++ my28brains/main_5_linear_vs_geodesic_distances.py
@@ -1,9 +1,20 @@
+"""Compare computation of linear vs geodesic square distances.
+
+This computation aims to show that it is not realistic to perform
+geodesic regression with the geodesic residuals.
+
+Note: the following paths in are my python path:
+/Users/ninamiolane/code/my28brains/H2_surfaceMatch:
+/Users/ninamiolane/code/my28brains:
+/Users/ninamiolane/code/:
+/Users/ninamiolane/opt/anaconda3/envs/my28brains/bin:
+/Users/ninamiolane/opt/anaconda3/envs/my28brains/lib/python3.10/site-packages
+"""
+
 import itertools
 import logging
 import os
 import time
-
-import torch
 
 os.environ["GEOMSTATS_BACKEND"] = "pytorch"
 import geomstats.backend as gs
@@ -29,24 +40,40 @@ def main_run(config):
     wandb_config.update(config)
     wandb.run.name = f"run_{wandb.run.id}"
     logging.info(f"\n\n---> MAIN 5 : START run: {wandb.run.name}.")
+    logging.info(wandb_config)
 
     reference_mesh = synthetic.generate_ellipsoid_mesh(
-        subdivisions=config.subdivisions, ellipse_dimensions=[2, 2, 3]
+        subdivisions=wandb_config.subdivisions, ellipse_dimensions=[2, 2, 3]
     )
+    reference_vertices = gs.array(reference_mesh.vertices)
     reference_faces = reference_mesh.faces
-    mesh = data_utils.add_noise(reference_mesh, config.noise_factor)
+    noisy_vertices = data_utils.add_noise(
+        mesh_sequence_vertices=[reference_vertices],
+        noise_factor=wandb_config.noise_factor,
+    )
+    noisy_vertices = noisy_vertices[0]
 
+    logging.info("Computing linear squared distance.")
     start = time.time()
-    linear_sq_dist = torch.linalg.norm(mesh - reference_mesh, axis=1) ** 2
+    linear_sq_dist = (
+        gs.linalg.norm(noisy_vertices - reference_vertices, axis=1).numpy() ** 2
+    )
     linear_duration = time.time() - start
+    logging.info(f"--> Done ({linear_duration} sec): {linear_sq_dist}...")
 
     discrete_surfaces = DiscreteSurfaces(faces=gs.array(reference_faces))
     elastic_metric = ElasticMetric(space=discrete_surfaces)
-    elastic_metric.exp_solver = _ExpSolver(n_steps=config.n_steps)
+    elastic_metric.exp_solver = _ExpSolver(n_steps=wandb_config.n_steps)
 
+    logging.info("Computing geodesic squared distance...")
     start = time.time()
-    geodesic_sq_dist = discrete_surfaces.metric.squared_dist(mesh, reference_mesh)
+    geodesic_sq_dist = (
+        discrete_surfaces.metric.squared_dist(noisy_vertices, reference_vertices)
+        .detach()
+        .numpy()
+    )
     geodesic_duration = time.time() - start
+    logging.info(f"--> Done ({geodesic_duration:.1f} sec): {geodesic_sq_dist:.3f}...")
 
     diff_sq_dist = linear_sq_dist - geodesic_sq_dist
     relative_diff_sq_dist = diff_sq_dist / linear_sq_dist
@@ -55,11 +82,11 @@ def main_run(config):
     wandb.log(
         {
             "run_name": wandb.run.name,
-            "noise_factor": config.noise_factor,
-            "n_steps": config.n_steps,
-            "subdivisions": config.subdivisions,
+            "noise_factor": wandb_config.noise_factor,
+            "n_steps": wandb_config.n_steps,
+            "subdivisions": wandb_config.subdivisions,
             "n_faces": len(reference_faces),
-            "n_vertices": len(reference_mesh.vertices),
+            "n_vertices": len(reference_vertices),
             "linear_sq_dist": linear_sq_dist,
             "geodesic_sq_dist": geodesic_sq_dist,
             "diff_sq_dist": diff_sq_dist,
@@ -68,6 +95,7 @@ def main_run(config):
             "geodesic_duration": geodesic_duration,
             "diff_duration": diff_duration,
             "relative_diff_duration": relative_diff_duration,
+            "reference_vertices": wandb.Object3D(reference_vertices.numpy()),
         }
     )
     wandb.finish()
