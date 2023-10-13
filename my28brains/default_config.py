5b7e6de40d186c2811f243e92f58bfb61874691a
Adele Myers <adele@frank.ece.ucsb.edu>
2023-10-13 15:00:05 -0700

    fix bug in main_1: paramtereization was treating day_range like an index (starting at 0), and sorting was treating day_range like actual days (starting at 1)

--- my28brains/default_config.py
+++ my28brains/default_config.py
@@ -96,7 +96,7 @@ template_day = 2
 # - we have 10 gpus, so we can run 10 interpolations at once
 # - they contain most of the progesterone peak.
 # first menstrual cycle is day 1-30 (pre-pill)
-day_range = [4, 12]  # we have parameterized meshes for days 2-11
+day_range = [2, 11]  # we have parameterized meshes for days 2-11
 
 run_interpolate = False
 
