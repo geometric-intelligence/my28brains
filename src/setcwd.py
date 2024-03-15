8f505896822961d656ce09f2a0c91149255ee3c8
Adele Myers <adele@ucsb.edu>
2024-03-15 11:34:00 -0700

    eccv figs

--- src/setcwd.py
+++ src/setcwd.py
@@ -36,13 +36,25 @@ def main():
 
     sys_dir = os.path.dirname(os.getcwd())
     sys.path.append(sys_dir)
-    print("Directory added to path: ", sys_dir)
+    print("Directory added to path: ", sys_dir) #my28brains
+    
     sys.path.append(os.getcwd())
-    print("Directory added to path: ", os.getcwd())
+    print("Directory added to path: ", os.getcwd()) #my28brains/src
+    notebook_dir = os.path.join(os.getcwd(), "notebooks")
+    print("Directory added to path: ", notebook_dir) #my28brains/src/notebooks
+    csv_dir = os.path.join(notebook_dir, "csv")
+    print("Directory added to path: ", csv_dir) #my28brains/src/notebooks/csv
+
     h2_dir = os.path.join(sys_dir, "H2_SurfaceMatch")
     sys.path.append(h2_dir)
-    print("Directory added to path: ", h2_dir)
-    notebook_dir = os.path.join(os.getcwd(), "notebooks")
-    print("Directory added to path: ", notebook_dir)
-    csv_dir = os.path.join(notebook_dir, "csv")
-    print("Directory added to path: ", csv_dir)
+    print("Directory added to path: ", h2_dir) #my28brains/H2_SurfaceMatch
+    
+    project_regression_dir = os.path.join(sys_dir, "project_regression")
+    sys.path.append(project_regression_dir)
+    print("Directory added to path: ", project_regression_dir) #my28brains/project_regression
+    project_regression_notebooks_dir = os.path.join(project_regression_dir, "notebooks")
+    sys.path.append(project_regression_notebooks_dir)
+    print("Directory added to path: ", project_regression_notebooks_dir) #my28brains/project_regression/notebooks
+    project_regression_csv_dir = os.path.join(project_regression_notebooks_dir, "csv")
+    print("Directory added to path: ", project_regression_csv_dir) #my28brains/project_regression/notebooks/csv
+
